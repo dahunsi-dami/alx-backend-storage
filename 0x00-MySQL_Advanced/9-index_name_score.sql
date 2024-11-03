b36ba5dae1c236f7d51d12981d1991379b3c3c0a
dahunsi-dami <gbemigadare@gmail.com>
2024-11-03 04:07:24 +0000

    Remove drop statement.

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -2,6 +2,4 @@
 -- the first letter of `name` and the `score`.
 -- Import given table dump.
 -- Only the first letter of `name` AND `score` must be indexed.
-DROP INDEX IF EXISTS idx_name_first_score ON names;
-
 CREATE INDEX idx_name_first_score ON names(name(1), score);
