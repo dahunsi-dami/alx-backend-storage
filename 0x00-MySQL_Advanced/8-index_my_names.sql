2c7685d0b038a327506f437aac6db7164487e2a1
dahunsi-dami <gbemigadare@gmail.com>
2024-11-03 03:24:29 +0000

    Create an index on table and 1st letter of name as specced.

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,6 @@
+-- Creates an index idx_name_first on table names & the first letter of name.
+-- import given table dump.
+-- Only first letter of name must be indexed.
+DROP INDEX IF EXISTS idx_name_first ON names;
+
+CREATE INDEX idx_name_first ON names (name(1));
