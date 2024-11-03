7be75e9933df0eae29009ad1a92960fedf83034c
dahunsi-dami <gbemigadare@gmail.com>
2024-11-03 03:57:30 +0000

    Remove drop statement & revert code back one commit.

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,6 +1,4 @@
 -- Creates an index idx_name_first on table names & the first letter of name.
 -- import given table dump.
 -- Only first letter of name must be indexed.
-DROP INDEX IF EXISTS idx_name_first;
-
-CREATE INDEX idx_name_first ON names(LEFT(name, 1));
+CREATE INDEX idx_name_first ON names(name(1));
